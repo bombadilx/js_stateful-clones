e3ef1d3beb31d29f9c0003b44c4a16adbac32f86
bombadilx <61424489+bombadilx@users.noreply.github.com>
2024-11-11 19:49:11 +0200

    Added new functionality

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,23 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+    for (const key in actions) {
+    switch (actions[key].type) {
+      case 'addProperties':
+        for (const extraKey in actions[key].extraData) {
+          state[extraKey] = actions[key].extraData[extraKey];
+        }
+        break;
+      case 'removeProperties':
+        for (const extraKey in actions[key].keysToRemove) {
+          delete state[actions[key].keysToRemove[extraKey]];
+        }
+        break;
+      case 'clear':
+        Object.keys(state).forEach(currentKey => delete state[currentKey]);
+        break;
+    }
+  }
 }
 
 module.exports = transformStateWithClones;
